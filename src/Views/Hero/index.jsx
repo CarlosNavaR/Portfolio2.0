5298dde09e0e14589708c344a4e64e7a07b59bdd
Buz <CarlosNava>
2023-01-16 14:35:08 -0800

    feat(HeroContent): :sparkles: add first hero content

--- src/Views/Hero/index.jsx
+++ src/Views/Hero/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Lottie from 'react-lottie';
 import { motion } from 'framer-motion';
 import { BiDownArrowAlt } from 'react-icons/bi';
+import useTranslate from '../../Hooks/useTranslate';
 import Astronaut from '../../Lotties/astronaut-hero.json';
 import './index.scss';
 
@@ -27,14 +28,9 @@ const defaultOptions = {
 
 const languagesClass = [
   {
-    name: 'React',
-    icon: 'devicon-react-plain',
-    color: '#61dafb',
-  },
-  {
-    name: 'Javascript',
-    icon: 'devicon-javascript-plain',
-    color: '#f7df1e',
+    name: 'Git',
+    icon: 'devicon-git-plain',
+    color: '#f05032',
   },
   {
     name: 'HTML5',
@@ -47,9 +43,19 @@ const languagesClass = [
     color: '#2965f1',
   },
   {
-    name: 'NodeJS',
-    icon: 'devicon-nodejs-plain',
-    color: '#339933',
+    name: 'Sass',
+    icon: 'devicon-sass-original',
+    color: '#cc6699',
+  },
+  {
+    name: 'React',
+    icon: 'devicon-react-plain',
+    color: '#61dafb',
+  },
+  {
+    name: 'Javascript',
+    icon: 'devicon-javascript-plain',
+    color: '#f7df1e',
   },
   {
     name: 'typescript',
@@ -57,18 +63,31 @@ const languagesClass = [
     color: '#007acc',
   },
   {
-    name: 'Sass',
-    icon: 'devicon-sass-original',
-    color: '#cc6699',
-  },
-  {
-    name: 'Git',
-    icon: 'devicon-git-plain',
-    color: '#f05032',
+    name: 'NodeJS',
+    icon: 'devicon-nodejs-plain',
+    color: '#339933',
   },
 ];
 
+const items = [
+  { id: 0, content: 'Buz🚀' },
+  { id: 1, content: 'Carlos' },
+];
+
 export default function index() {
+  const [current, setCurrent] = useState(0);
+  const textTranslate = "Hi, I'm";
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCurrent(state => {
+        if (state >= items.length - 1) return 0;
+        return state + 1;
+      });
+    }, 2000);
+    return () => clearInterval(id);
+  }, []);
+
   return (
     <div className='hero'>
       <div className='Content'>
@@ -88,6 +107,32 @@ export default function index() {
         </motion.div>
         <div className='hero_img'>
           <Lottie options={defaultOptions} height={300} width={300} />
+        </div>
+
+        <div className='hero_about'>
+          <div className='hero_about_title'>
+            {useTranslate(textTranslate)}
+            <motion.div
+              key={items[current].id}
+              initial={{ opacity: 0, y: -30 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 30 }}
+              transition={{
+                type: 'spring',
+                duration: 1.5,
+                ease: 'anticipate',
+              }}
+            >
+              <span id='flip'>{items[current].content}</span>
+            </motion.div>
+          </div>
+
+          <h2 className='hero_about_subtitle'>
+            <span className='hero_subtitle_name'>
+              {' '}
+              {useTranslate('Jr Frontend Developer')}
+            </span>
+          </h2>
         </div>
       </div>
       <div className='scroll_down'>
