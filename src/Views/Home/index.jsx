d7fb6d243f10fc1b0adad8ae54bdab228dada99d
Buz <carlosnavareyes1@gmail.com>
2023-02-11 22:18:00 -0800

    fix: :lipstick: add max with to avoid large text in large screen

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { motion, useScroll, useSpring } from 'framer-motion';
 import { Nav, Footer } from '../../Components';
 import About from '../About';
 import Hero from '../Hero';
@@ -8,14 +9,28 @@ import Contact from '../Contact';
 export default function index() {
   const component = useRef();
 
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
-    <div className='App' ref={component}>
-      <Nav />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <motion.div className='progress_bar' style={{ scaleX }} />
+      <div className='App' ref={component}>
+        <Nav />
+        <div className='start_container'>
+          <Hero />
+        </div>
+        <div className='App_container'>
+          <About />
+          <Projects />
+          <Contact />
+        </div>
+        <Footer />
+      </div>
+    </>
   );
 }
