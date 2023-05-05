3aeab12dd3d18508ea24329237d79eda84635ad5
Buz <carlosnavareyes1@gmail.com>
2023-05-04 22:37:59 -0700

    fix: process.env missing in client side

--- next.config.js
+++ next.config.js
@@ -9,5 +9,7 @@ module.exports = {
   trailingSlash: true,
   env: {
     IMAGE_SANITY_URL: process.env.IMAGE_SANITY_URL,
+    APP_EMAIL: process.env.APP_EMAIL,
+    APP_LINK_CV: process.env.APP_LINK_CV,
   }
 };
