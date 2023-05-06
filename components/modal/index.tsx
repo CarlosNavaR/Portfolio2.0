3d430063c19046837215db5954f0e7121c275ca9
Buz <carlosnavareyes1@gmail.com>
2023-05-06 11:59:06 -0700

    feat: new implementation and remove animation from global theme

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { ModalPropsType } from '@/types/modal';
 import Styles from './index.module.scss';
+
+const dropIn = {
+  hidden: {
+    y: '100vh',
+    opacity: 0,
+  },
+  visible: {
+    y: '0',
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      type: 'tween',
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: '100vh',
+    opacity: 0,
+  },
+};
 
 export default function index(props: ModalPropsType) {
   return (
     <>
       {props.visible && (
-        <div
+        <motion.div
           className={`${Styles.modal} ${
             props.hasOverlay ? Styles.overlay : ''
           }`}
           onClick={props.hasOverlay ? props.onClose : () => {}}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
         >
-          <div className={Styles.modal_content}>
+          <motion.div
+            className={Styles.modal_content}
+            variants={dropIn}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+          >
             <div className={Styles.modal_header}>
               <span className={Styles.modal_title}>{props.title}</span>
               {props.closable && (
@@ -25,8 +56,8 @@ export default function index(props: ModalPropsType) {
             {props.footer && (
               <div className={Styles.modal_footer}>{props.footer}</div>
             )}
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       )}
     </>
   );
